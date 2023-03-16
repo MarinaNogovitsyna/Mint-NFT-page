9fb386541282739ec9f41b7f3ca5305fe3c55ec4
Marina <marina.nogovitsyna.98@mail.ru>
2023-03-16 12:50:30 +0300

    background

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-
+VANTA.FOG({
+    el: "#main-background",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x4565b0,
+    midtoneColor: 0xffab00,
+    blurFactor: 0.54,
+    speed: 1.50,
+    zoom: 1.30
+  })
 
 
 
