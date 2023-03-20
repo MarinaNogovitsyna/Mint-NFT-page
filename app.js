fbc84d35090fd50e5631f189e4ae7cc3f6aa56f0
Marina <marina.nogovitsyna.98@mail.ru>
2023-03-20 19:29:05 +0300

    +

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 VANTA.FOG({
-    //el: "#main-background",
+    el: "#main-background",
     mouseControls: true,
     touchControls: true,
     gyroControls: false,
@@ -14,21 +14,42 @@ VANTA.FOG({
 
 
 
+// const slides = document.querySelectorAll('.slide')
+
+// for (const slide of slides) {
+//     slide.addEventListener('click', () => {
+//         clearActiveClasses()
+
+//         slide.classList.add('active')
+//     })
+// }
+
+// function clearActiveClasses() {
+//     slides.forEach((slide) => {
+//         slide.classList.remove('active')
+//     })
+// }
+
 const slides = document.querySelectorAll('.slide')
+let activeSlideIndex = 0
 
-for (const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearActiveClasses()
-
-        slide.classList.add('active')
-    })
-}
+// Вызываем функцию автоматической смены слайдов каждые 3 секунды
+setInterval(() => {
+  clearActiveClasses()
+  activeSlideIndex++
+  if (activeSlideIndex === slides.length) {
+    activeSlideIndex = 0
+  }
+  slides[activeSlideIndex].classList.add('active')
+}, 3000)
 
 function clearActiveClasses() {
-    slides.forEach((slide) => {
-        slide.classList.remove('active')
-    })
+  slides.forEach((slide) => {
+    slide.classList.remove('active')
+  })
 }
+
+
 
 
 
@@ -68,3 +89,35 @@ async function connect() {
      console.log("No wallet");
     }
    }
+
+
+   // Предполагаемый код для зачисления ETH:
+//    async function connect() {
+//     if (window.ethereum) {
+//       await window.ethereum.request({ method: "eth_requestAccounts" });
+//       window.web3 = new Web3(window.ethereum);
+//       document.getElementById("buttonMM").value = "Mint NFT";
+  
+//       // Add event listener to the button
+//       const mintButton = document.getElementById("buttonMM");
+//       mintButton.addEventListener("click", async () => {
+//           // Get the user's Ethereum address
+//           const accounts = await window.web3.eth.getAccounts();
+//           const userAddress = accounts[0];
+  
+//           // Create a transaction object to transfer 0.001 ETH to your address
+//           const transactionObject = {
+//               from: userAddress,
+//               to: "YOUR_ADDRESS",
+//               value: window.web3.utils.toWei("0.001", "ether")
+//           };
+  
+//           // Send the transaction
+//           const transactionHash = await window.web3.eth.sendTransaction(transactionObject);
+//           console.log(`Transaction hash: ${transactionHash}`);
+//       });
+  
+//   } else {
+//       console.log("No wallet");
+//   }
+// }
